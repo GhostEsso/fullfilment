986388e777594de0bb4baac6ba3d5da8cffafad0
Essohanam TAMBANA <philippe.tambana@kaba-delivery.com>
2026-05-06 07:29:29 +0000

    feat: add Meta Pixel tracking code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -129,6 +129,28 @@ export default function RootLayout({
             gtag('config', 'G-8VNH29R4QL');
           `}
         </Script>
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '2240382536703817');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img 
+            height="1" 
+            width="1" 
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=2240382536703817&ev=PageView&noscript=1"
+          />
+        </noscript>
         <script
           id="business-schema"
           type="application/ld+json"
